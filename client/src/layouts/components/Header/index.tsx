b23f885bdc9ec3e244b85dee625738afce0496ff
ThienBinhT9 <phongzeros3@gmail.com>
2024-07-10 09:19:48 +0700

    update code

--- client/src/layouts/components/Header/index.tsx
+++ client/src/layouts/components/Header/index.tsx
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import { useDispatch, useSelector } from "react-redux";
-import { Dropdown, MenuProps, Drawer, Menu } from "antd";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Dropdown, MenuProps, Drawer, Badge } from "antd";
+import OutsideClickHandler from "react-outside-click-handler";
 import {
   UserOutlined,
-  LogoutOutlined,
   MenuOutlined,
+  BellOutlined,
+  LogoutOutlined,
   SearchOutlined,
+  MessageOutlined,
+  CloseCircleOutlined,
 } from "@ant-design/icons";
 
 import "./Header.scss";
-import { NavLink, useNavigate } from "react-router-dom";
 import { RootState } from "../../../redux/store";
 import logo from "../../../assets/images/logo.png";
 import { signOut } from "../../../services/auth-service.ts";
 
+import Input from "../../../components/Input/index.tsx";
 import Button from "../../../components/Button/index.tsx";
 import ThreeDotLoader from "../../../components/Loading/ThreeDot.tsx";
 
-import { jwtDecode } from "jwt-decode";
-
 function Header() {
   const { token, loading } = useSelector((state: RootState) => state.auth);
+  const isSmallScreen = useMediaQuery({ maxWidth: 768 });
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const drawerRef = useRef<HTMLDivElement>(null);
 
-  const [openDrawer, setOpenDrawer] = useState(false);
-
-  const showDrawer = () => {
-    setOpenDrawer(true);
-  };
+  const [openDrawerMenu, setOpenDrawerMenu] = useState(false);
+  const [openDrawerSearch, setOpenDrawerSearch] = useState(false);
 
   const onClose = () => {
-    setOpenDrawer(false);
+    setOpenDrawerMenu(false);
+    setOpenDrawerSearch(false);
   };
 
   const handleSignOut = async () => {
@@ -56,6 +60,10 @@ function Header() {
     },
   ];
 
+  useEffect(() => {
+    isSmallScreen ? setOpenDrawerSearch(false) : setOpenDrawerMenu(false);
+  }, [isSmallScreen]);
+
   return (
     <div className="wrapper-header">
       {loading && <ThreeDotLoader />}
@@ -66,6 +74,60 @@ function Header() {
         {token.access_token ? (
           <>
             <div className="header-nav">
+              <OutsideClickHandler onOutsideClick={onClose}>
+                <SearchOutlined
+                  className="header-nav-item"
+                  onClick={() => setOpenDrawerSearch(true)}
+                />
+                <div ref={drawerRef}>
+                  <Drawer
+                    mask={false}
+                    placement="right"
+                    closable={false}
+                    onClose={onClose}
+                    open={openDrawerSearch}
+                    getContainer={() =>
+                      drawerRef.current?.parentElement || document.body
+                    }
+                    title={
+                      <div>
+                        <h4 className="drawer-title-text">Tìm kiếm</h4>
+                        <Input
+                          placeholder="Tìm kiếm"
+                          prefix={<SearchOutlined />}
+                          suffix={<CloseCircleOutlined />}
+                          style={{ backgroundColor: "#efefef", border: "none" }}
+                        />
+                      </div>
+                    }
+                  >
+                    <p style={{ fontSize: 15, fontWeight: 500 }}>Gần đây</p>
+                  </Drawer>
+                </div>
+              </OutsideClickHandler>
+
+              <Dropdown
+                menu={{ items }}
+                trigger={["click"]}
+                placement="bottomRight"
+                arrow={{ pointAtCenter: true }}
+                className="header-dropdown"
+              >
+                <Badge dot size="small">
+                  <MessageOutlined className="header-nav-item" />
+                </Badge>
+              </Dropdown>
+              <Dropdown
+                menu={{ items }}
+                trigger={["click"]}
+                arrow={{ pointAtCenter: true }}
+                placement="bottomRight"
+                className="header-dropdown"
+              >
+                <Badge dot size="small">
+                  <BellOutlined className="header-nav-item" />
+                </Badge>
+              </Dropdown>
               <Dropdown
                 menu={{ items }}
                 trigger={["click"]}
@@ -74,17 +136,20 @@ function Header() {
                 className="header-dropdown"
               >
                 <div className="header-nav-item--more">
-                  <img src="https://i.pinimg.com/564x/8f/5a/90/8f5a90b9dbf868baf53fc990cde10e6e.jpg" />
+                  <img
+                    src="https://i.pinimg.com/564x/8f/5a/90/8f5a90b9dbf868baf53fc990cde10e6e.jpg"
+                    alt="avatar"
+                  />
                 </div>
               </Dropdown>
             </div>
             <div className="header-nav-mobile">
               <SearchOutlined />
-              <MenuOutlined onClick={showDrawer} />
+              <MenuOutlined onClick={() => setOpenDrawerMenu(true)} />
               <Drawer
                 width="65%"
                 onClose={onClose}
-                open={openDrawer}
+                open={openDrawerMenu}
                 closable={false}
                 title="Menu"
                 footer={
